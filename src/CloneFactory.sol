c526b875e11703a17f6221b190ad8cc61e34685d
sammyshakes <sam.shakespeare@gmail.com>
2023-08-11 11:43:16 -0400

    added comments to clonefactory

--- src/CloneFactory.sol
+++ src/CloneFactory.sol
@@ -6,6 +6,8 @@ import "./ERC1155Cloneable.sol";
 import "./ERC721CloneableTBA.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
+/// @title CloneFactory
+/// @notice A contract for cloning and managing ERC721 and ERC1155 contracts.
 contract CloneFactory {
     event CloneCreated(address clone, string name);
 
@@ -23,6 +25,12 @@ contract CloneFactory {
     mapping(uint256 => address) public erc1155Clones;
     mapping(uint256 => address) public erc721Clones;
 
+    /// @notice Constructs the CloneFactory contract.
+    /// @param _tronicAdmin The address of the Tronic admin.
+    /// @param _erc721Implementation The address of the ERC721 implementation.
+    /// @param _erc1155implementation The address of the ERC1155 implementation.
+    /// @param _registry The address of the registry contract.
+    /// @param _accountImplementation The address of the account implementation.
     constructor(
         address _tronicAdmin,
         address _erc721Implementation,
@@ -38,16 +46,24 @@ contract CloneFactory {
         accountImplementation = payable(_accountImplementation);
     }
 
+    /// @dev Modifier to require that the caller is the owner of the contract.
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not owner");
         _;
     }
 
+    /// @dev Modifier to require that the caller is the Tronic admin.
     modifier onlyTronicAdmin() {
         require(msg.sender == tronicAdmin, "Caller is not Tronic admin");
         _;
     }
 
+    /// @notice Clones the ERC1155 implementation and initializes it.
+    /// @param uri The URI for the cloned contract.
+    /// @param admin The address of the admin for the cloned contract.
+    /// @param name The name of the token.
+    /// @param symbol The symbol of the token.
+    /// @return erc1155cloneAddress The address of the newly cloned ERC1155 contract.
     function cloneERC1155(
         string memory uri,
         address admin,
@@ -64,6 +80,12 @@ contract CloneFactory {
         emit CloneCreated(erc1155cloneAddress, uri);
     }
 
+    /// @notice Clones the ERC721 implementation and initializes it.
+    /// @param name The name of the token.
+    /// @param symbol The symbol of the token.
+    /// @param uri The URI for the cloned contract.
+    /// @param admin The address of the admin for the cloned contract.
+    /// @return erc721CloneAddress The address of the newly cloned ERC721 contract.
     function cloneERC721(string memory name, string memory symbol, string memory uri, address admin)
         external
         onlyTronicAdmin
@@ -80,39 +102,58 @@ contract CloneFactory {
         emit CloneCreated(erc721CloneAddress, uri);
     }
 
+    /// @notice Retrieves the address of a specific ERC1155 clone by its index.
+    /// @param index The index of the ERC1155 clone.
+    /// @return The address of the ERC1155 clone.
     function getERC1155Clone(uint256 index) external view returns (address) {
         return erc1155Clones[index];
     }
 
+    /// @notice Retrieves the address of a specific ERC721 clone by its index.
+    /// @param index The index of the ERC721 clone.
+    /// @return The address of the ERC721 clone.
     function getERC721Clone(uint256 index) external view returns (address) {
         return erc721Clones[index];
     }
 
+    /// @notice Retrieves the total number of ERC1155 clones.
+    /// @return The total number of ERC1155 clones.
     function getNumERC1155Clones() external view returns (uint256) {
         return _numERC1155Clones;
     }
 
+    /// @notice Retrieves the total number of ERC721 clones.
+    /// @return The total number of ERC721 clones.
     function getNumERC721Clones() external view returns (uint256) {
         return _numERC721Clones;
     }
 
-    function setTronicAdmin(address newAdmin) external onlyOwner {
-        require(msg.sender == tronicAdmin, "Caller is not the tronicAdmin");
+    /// @notice Sets the Tronic admin address, callable only by the tronic admin.
+    /// @param newAdmin The address of the new Tronic admin.
+    function setTronicAdmin(address newAdmin) external onlyTronicAdmin {
         tronicAdmin = newAdmin;
     }
 
+    /// @notice Sets the ERC721 implementation address, callable only by the owner.
+    /// @param newImplementation The address of the new ERC721 implementation.
     function setERC721Implementation(address newImplementation) external onlyOwner {
         erc721Implementation = ERC721CloneableTBA(newImplementation);
     }
 
+    /// @notice Sets the ERC1155 implementation address, callable only by the owner.
+    /// @param newImplementation The address of the new ERC1155 implementation.
     function setERC1155Implementation(address newImplementation) external onlyOwner {
         erc1155implementation = ERC1155Cloneable(newImplementation);
     }
 
+    /// @notice Sets the account implementation address, callable only by the owner.
+    /// @param newImplementation The address of the new account implementation.
     function setAccountImplementation(address payable newImplementation) external onlyOwner {
         accountImplementation = newImplementation;
     }
 
+    /// @notice Sets the registry address, callable only by the owner.
+    /// @param newRegistry The address of the new registry.
     function setRegistry(address newRegistry) external onlyOwner {
         registry = IERC6551Registry(newRegistry);
     }
